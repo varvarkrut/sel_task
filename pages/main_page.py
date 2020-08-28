d9947fba7e0089fa0735712522e7c981b2d5b01c
e1_name <vanya.shcurko@gmail.com>
2020-08-28 23:37:23 +0300

    fixed except (added NoSuchElementException) into test_items.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,10 @@
 from selenium.webdriver.common.by import By
-from base_page import BasePage
+import sys
+print(sys.path)
+from pages.base_page import BasePage
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+    def should_be_login_link(self):
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
