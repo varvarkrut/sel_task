5844b7732bb2ca53360e3b5c7a9f3748266d472c
e1_name <vanya.shcurko@gmail.com>
2020-08-29 21:16:25 +0300

    added locators.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,12 @@
-from selenium.webdriver.common.by import By
-import sys
-print(sys.path)
 from pages.base_page import BasePage
+from pages.locators import MainPageLocators
+from selenium.webdriver.common.by import By
+
+
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+
     def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK)
