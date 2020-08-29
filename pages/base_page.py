5fc00046883fbadd109de94099b5b48d761530ec
e1_name <vanya.shcurko@gmail.com>
2020-08-29 22:52:37 +0300

    fixed print in base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,7 +4,6 @@ from selenium.common.exceptions import NoSuchElementException
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
-        print(dir(browser))
         self.url = url
         self.browser.implicitly_wait(10)
 
