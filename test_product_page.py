99315a7ee79ea486e5a47b541d4adb2db82fcd78
e1_name <vanya.shcurko@gmail.com>
2020-08-30 16:04:34 +0300

    added links into basket test at product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,14 @@
 from pages.product_page import ProductPage
 import math
-from selenium.common.exceptions import NoAlertPresentException  # в начале файла
+from selenium.common.exceptions import NoAlertPresentException
+import pytest
+import time
 
-
-
-
-def test_guest_can_add_product_to_basket(browser):
-    link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
+                                  pytest.param(7, marks=pytest.mark.xfail),
+                                  8, 9])
+def test_guest_can_add_product_to_basket(browser, link):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
     page = ProductPage(browser, link)
     page.open()
     page.click_on_the_basket()
