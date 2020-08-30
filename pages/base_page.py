32d60bc1e632dc6456886c6ed9c787edf922bc96
e1_name <vanya.shcurko@gmail.com>
2020-08-31 01:20:01 +0300

    added login tests into test_product_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,12 +2,18 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pages.locators import BasePageLocators
+
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(10)
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
 
     def open(self):
         self.browser.get(self.url)
@@ -18,21 +24,24 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
-    
+
     def find_element(self, how, what):
         try:
             return self.browser.find_element(how, what)
         except NoSuchElementException:
             return None
-    
+
     def is_not_element_present(self, how, what, timeout=4):
         try:
-            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+            WebDriverWait(
+                self.browser, timeout).until(
+                EC.presence_of_element_located(
+                    (how, what)))
         except TimeoutException:
             return True
 
         return False
-    
+
     def is_disappeared(self, how, what, timeout=4):
         try:
             WebDriverWait(self.browser, timeout, 1, TimeoutException).\
@@ -42,3 +51,6 @@ class BasePage():
 
         return True
 
+    def should_be_login_link(self):
+        assert self.is_element_present(
+            *BasePageLocators.LOGIN_LINK), "Login link is not presented"
